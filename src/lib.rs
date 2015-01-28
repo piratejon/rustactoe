e4928b7d491b0139eabecb9019a02c20574c6a90
Jonathan Wesley Stone <js@piratejon.com>
2015-01-27 22:59:15 -0600

    spare the caller the burden of knowing anything about the internal representation

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,16 @@ pub struct TttSquare {
   pub value : char,
 }
 
+impl std::default::Default for TttSquare {
+  fn default() -> TttSquare {
+    TttSquare {
+      value : 'x',
+    }
+  }
+}
+
 impl TttSquare {
   pub fn set_x(&mut self) { self.value = 'x'; }
-  pub fn get_value(&self) -> char { return 'x'; }
+  pub fn get_value(&self) -> char { 'x' }
 }
 
--- tests/lib.rs
+++ tests/lib.rs
@@ -8,7 +8,7 @@ fn test_it() {
 
 #[test]
 fn tttsquare() {
-  let mut square = rustactoe::TttSquare { value: 'x' };
+  let mut square : rustactoe::TttSquare = std::default::Default::default();
   square.set_x();
   assert_eq!(square.get_value(), 'x');
 }
