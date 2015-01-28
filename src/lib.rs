6d80bef37f5c0cb2b7294c9d9e39a5c97dd0c162
Jonathan Wesley Stone <js@piratejon.com>
2015-01-27 22:48:38 -0600

    green struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
-#[test]
-fn it_works() {
-}
+
+pub struct TttSquare;
+
---
+++ tests/lib.rs
@@ -0,0 +1,13 @@
+
+extern crate rustactoe;
+
+#[test]
+fn test_it() {
+  assert!(true);
+}
+
+#[test]
+fn tttsquare() {
+  let square = rustactoe::TttSquare;
+}
+
