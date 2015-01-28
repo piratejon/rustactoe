6434981a9651bb5cf4c4821c92db32649bd20591
Jonathan Wesley Stone <js@piratejon.com>
2015-01-27 23:08:08 -0600

    changed some implementation details with the user none the wiser

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,22 @@
 
+enum TttSquareValue {
+  X, O, Blank
+}
+
 pub struct TttSquare {
-  pub value : char,
+  value : TttSquareValue,
 }
 
 impl std::default::Default for TttSquare {
   fn default() -> TttSquare {
     TttSquare {
-      value : 'x',
+      value : TttSquareValue::X,
     }
   }
 }
 
 impl TttSquare {
-  pub fn set_x(&mut self) { self.value = 'x'; }
+  pub fn set_x(&mut self) { self.value = TttSquareValue::X; }
   pub fn get_value(&self) -> char { 'x' }
 }
 
