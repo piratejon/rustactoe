a9aed9212c3c4ef9ce34f2ca17d0aa44a190c42d
Jonathan Wesley Stone <js@piratejon.com>
2015-01-31 15:22:04 -0600

    failing binrep test

--- tests/lib.rs
+++ tests/lib.rs
@@ -103,3 +103,73 @@ fn tttboard() {
   assert_eq!(board.winner(), 'x');
 }
 
+#[test]
+fn tttboard_binrep() {
+  let mut board : rustactoe::TttBoardBinRep = std::default::Default::default();
+  /*
+  assert_eq!(board.count_blanks(), 9);
+
+  assert_eq!(board.get_square(1), '_');
+  assert_eq!(board.get_square(2), '_');
+  assert_eq!(board.get_square(3), '_');
+  assert_eq!(board.get_square(4), '_');
+  assert_eq!(board.get_square(5), '_');
+  assert_eq!(board.get_square(6), '_');
+  assert_eq!(board.get_square(7), '_');
+  assert_eq!(board.get_square(8), '_');
+  assert_eq!(board.get_square(9), '_');
+
+  board.set_square(8, 'x');
+  assert_eq!(board.get_square(8), 'x');
+  assert_eq!(board.count_blanks(), 8);
+
+  board.set_square(1, ' ');
+  assert_eq!(board.get_square(1), '_');
+    /*
+       1 | 2 | 3
+       ---------
+       4 | 5 | 6
+       ---------
+       7 | 8 | 9
+       */
+  board.set_square(9, 'x');
+  assert_eq!(board.count_blanks(), 7);
+  assert_eq!(board.winner(), '_');
+  board.set_square(7, 'x');
+  assert_eq!(board.count_blanks(), 6);
+  assert_eq!(board.winner(), 'x');
+
+  board.reset();
+  assert_eq!(board.count_blanks(), 9);
+  assert_eq!(board.get_square(1), '_');
+  assert_eq!(board.get_square(2), '_');
+  assert_eq!(board.get_square(3), '_');
+  assert_eq!(board.get_square(4), '_');
+  assert_eq!(board.get_square(5), '_');
+  assert_eq!(board.get_square(6), '_');
+  assert_eq!(board.get_square(7), '_');
+  assert_eq!(board.get_square(8), '_');
+  assert_eq!(board.get_square(9), '_');
+
+  board.set_square(1, 'o');
+  assert_eq!(board.count_blanks(), 8);
+  assert_eq!(board.winner(), '_');
+  board.set_square(5, 'o');
+  assert_eq!(board.count_blanks(), 7);
+  assert_eq!(board.winner(), '_');
+  board.set_square(9, 'o');
+  assert_eq!(board.count_blanks(), 6);
+  assert_eq!(board.winner(), 'o');
+
+  board.set_square(1, 'x');
+  assert_eq!(board.count_blanks(), 6);
+  assert_eq!(board.winner(), '_');
+  board.set_square(5, 'x');
+  assert_eq!(board.count_blanks(), 6);
+  assert_eq!(board.winner(), '_');
+  board.set_square(9, 'x');
+  assert_eq!(board.count_blanks(), 6);
+  assert_eq!(board.winner(), 'x');
+  */
+}
+
